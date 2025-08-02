a9af96d69ab93ff7aceee7d695c3847504c30d96
hexo-baoge <wangsir666998@outlook.com>
2025-08-02 10:47:22 +0800

    Auto update: 08周六2025 1047

--- themes/solitude/scripts/event/cdn.js
+++ themes/solitude/scripts/event/cdn.js
@@ -26,6 +26,7 @@ hexo.extend.filter.register("before_generate", () => {
     cover_api: { name, file: "js/covercolor/api.js", version },
     cover_ave: { name, file: "js/covercolor/ave.js", version },
     music_js: { name, file: "js/music.js", version },
+    local_music_js: { name, file: "js/local-music.js", version },
     right_menu_js: { name, file: "js/right_menu.js", version },
     translate_js: { name, file: "js/tw_cn.js", version },
     commentBarrage: { name, file: "js/third_party/barrage.min.js", version },
